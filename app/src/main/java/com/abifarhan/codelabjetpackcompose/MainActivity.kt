d76ca309653cfd65ba8cb00955a51f03b76661a8
abiFarhan <abifarhan04@gmail.com>
2021-11-17 09:42:10 +0700

    Implement column with component design part 4

--- app/src/main/java/com/abifarhan/codelabjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/abifarhan/codelabjetpackcompose/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.abifarhan.codelabjetpackcompose.ui.theme.CodeLabJetpackComposeTheme
@@ -56,17 +56,17 @@ fun Greeting(name: String) {
         modifier = Modifier.padding(vertical = 4.dp,
         horizontal = 8.dp)
         ) {
-        Column(modifier = Modifier
-            .fillMaxWidth()
-            .padding(24.dp)) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
 //            Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
-            Text(text = "Hello,")
-            Text(text = name)
-            
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "Show less")
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { /*TODO*/ }, ) {
+                Text(text = "Show more")
             }
         }
+
     }
 //    Image()
 }
