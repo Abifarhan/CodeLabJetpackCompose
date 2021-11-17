7c241df08573e9fa002ee5c8354c13abd9a90859
abiFarhan <abifarhan04@gmail.com>
2021-11-17 09:48:02 +0700

    Implement column with component design part 6

--- app/src/main/java/com/abifarhan/codelabjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/abifarhan/codelabjetpackcompose/MainActivity.kt
@@ -53,16 +53,21 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
 
 @Composable
 fun Greeting(name: String) {
-    var expanded = remember{ mutableStateOf(false)} //Don't do this
+    var expanded = remember { mutableStateOf(false) } //Don't do this
+
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
 
     Surface(
         color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp,
-        horizontal = 8.dp)
-        ) {
+        modifier = Modifier.padding(
+            vertical = 4.dp,
+            horizontal = 8.dp
+        )
+    ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
-//            Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+            Column(modifier = Modifier.weight(1f)
+                .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
