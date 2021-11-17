6b1fbe4715d168603cc495934887c5f22aa26fe3
abiFarhan <abifarhan04@gmail.com>
2021-11-17 09:28:02 +0700

    Implement column with component design part 2

--- app/src/main/java/com/abifarhan/codelabjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/abifarhan/codelabjetpackcompose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -31,8 +32,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp(names: List<String> = listOf("World","Compose")) {
-    
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+
     Column {
         for (name in names) {
             Greeting(name = name)
@@ -49,8 +50,12 @@ private fun MyApp(names: List<String> = listOf("World","Compose")) {
 
 @Composable
 fun Greeting(name: String) {
-    androidx.compose.material.Surface(color = MaterialTheme.colors.primary) {
-        Column(modifier = Modifier.padding(24.dp)) {
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp,
+        horizontal = 8.dp)
+        ) {
+        Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
 //            Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
             Text(text = "Hello,")
             Text(text = name)
